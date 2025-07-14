5d4bddbe7efcb5749022702df01b21c81e359f0c
wlals2 <fw4568@gmail.com>
2025-07-14 18:01:18 +0900

    과제 커밋

---
+++ codes/cd_pwd_ls.sh
@@ -0,0 +1,9 @@
+[ohjimin@localhost Downloads]$ cd /home/ohjimin/Pictures/
+[ohjimin@localhost Pictures]$ pwd
+/home/ohjimin/Pictures
+[ohjimin@localhost Pictures]$ ls
+[ohjimin@localhost Pictures]$ ^C
+[ohjimin@localhost Pictures]$ cd /home/ohjimin/Music/
+[ohjimin@localhost Music]$ pwd
+/home/ohjimin/Music
+[ohjimin@localhost Music]$ ls
---
+++ codes/test.sh
@@ -0,0 +1,8 @@
+[ohjimin@localhost Downloads]$ cd /home/ohjimin/Pictures/
+[ohjimin@localhost Pictures]$ pwd
+/home/ohjimin/Pictures
+[ohjimin@localhost Pictures]$ ls
+[ohjimin@localhost Pictures]$ cd /home/ohjimin/Music/
+[ohjimin@localhost Music]$ pwd
+/home/ohjimin/Music
+[ohjimin@localhost Music]$ ls
